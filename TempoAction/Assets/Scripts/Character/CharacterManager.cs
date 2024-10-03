f419195d7ca25101290dcddb99b2795775f854d9
Budy123 <kth5948@naver.com>
2024-10-04 05:37:02 +0900

    Fix: 인스펙터에서 캐릭터를 할당할 수 있도록 변경 및 레이어 오류 수정

--- TempoAction/Assets/Scripts/Character/CharacterManager.cs
+++ TempoAction/Assets/Scripts/Character/CharacterManager.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CharacterManager : MonoBehaviour
+public class CharacterManager : Singleton<CharacterManager>
 {
-    public static List<GameObject> characters = new List<GameObject>();
+    public List<GameObject> characters = new List<GameObject>();
 
-    public static List<GameObject> GetCharacter(int layerMask)
+    public List<GameObject> GetCharacter(int layerMask)
     {
        List<GameObject> list = new List<GameObject>();
 
-        list = characters.Where((obj) => (obj.layer & layerMask) != 0).ToList();
+        list = characters.Where((obj) => (1 << obj.layer & layerMask) != 0).ToList();
         
         return list;
     }
