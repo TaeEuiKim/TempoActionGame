e39d1be4fae2a5eb5230c59c662444dab8d4f80f
DESKTOP-TE0OEVF\Hyeon2-400 <ib041615@naver.com>
2024-10-04 21:32:01 +0900

    fix attack

--- TempoAction/Assets/Scripts/Player/Attack/PlayerAttack.cs
+++ TempoAction/Assets/Scripts/Player/Attack/PlayerAttack.cs
@@ -133,7 +133,7 @@ public class PlayerAttack
     {
         while (_player.Stat.Hp > 0)
         {
-            yield return new WaitForSeconds(0.4f);
+            yield return new WaitForSeconds(0.6f);
 
             if (!isAttack)
             {
