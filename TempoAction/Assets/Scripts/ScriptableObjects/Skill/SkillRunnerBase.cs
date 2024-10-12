c6585264b9a5c7277939bace31f726660c16f399
Budy123 <kth5948@naver.com>
2024-10-12 15:08:34 +0900

    Add : SkillRunnerBase 신규 메소드 추가 및 주석(Summary) 추가

--- TempoAction/Assets/Scripts/ScriptableObjects/Skill/SkillRunnerBase.cs
+++ TempoAction/Assets/Scripts/ScriptableObjects/Skill/SkillRunnerBase.cs
@@ -1,26 +1,46 @@
-using System.Linq;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.TextCore.Text;
 
 public abstract class SkillRunnerBase : ScriptableObject
 {
     public SkillData skillData;
     protected List<ParticleSystem> managedEffects = new List<ParticleSystem>();
+    protected WaitForSeconds preDelayWFS;
 
     public virtual void Run(CharacterBase character, UnityAction OnEnded = null)
     {
         Initialize();
         character.StartCoroutine(WaitForSkillEnded(character, OnEnded));
     }
-    public abstract void Initialize();
+    
+    /// <summary>
+    /// 초기화 메소드. 
+    /// 이펙트 등의 메소드를 초기화 하는 용도. 
+    /// *//preDelayWFS를 사용하려면 반드시 오버라이드 이후 호출 필요//*
+    /// </summary>
+    public virtual void Initialize()
+    {
+        if (preDelayWFS == null)
+        {
+            preDelayWFS = new WaitForSeconds(skillData.SkillCastingTime * SkillData.Time2Second);
+        }
+    }
+
+    /// <summary>
+    /// 실제로 스킬을 사용하는 부분.
+    /// 스킬을 사용했을 때 발생될 행동을 정의하는 코루틴
+    /// </summary>
+    /// <param name="character">스킬 사용자(캐릭터)</param>
+    /// <returns></returns>
     public abstract IEnumerator SkillCoroutine(CharacterBase character);
+
     private IEnumerator WaitForSkillEnded(CharacterBase character, UnityAction OnFinished)
     {
-        while(true)
+        while (true)
         {
             yield return SkillCoroutine(character);
 
@@ -43,7 +63,7 @@ public abstract class SkillRunnerBase : ScriptableObject
             removingEffects.Clear();
             foreach (var effect in effects)
             {
-                if(!effect.IsAlive(true)) { removingEffects.Add(effect); }
+                if (!effect.IsAlive(true)) { removingEffects.Add(effect); }
             }
 
             // 죽은 이펙트는 비활성화 및 더이상 검사하지 않도록 삭제
@@ -54,6 +74,12 @@ public abstract class SkillRunnerBase : ScriptableObject
             }
         }
     }
+
+    /// <summary>
+    /// character의 위치에 effect를 활성화한다.
+    /// </summary>
+    /// <param name="character">대상 캐릭터</param>
+    /// <param name="effect">대상 이펙트</param>
     protected void ActiveEffectToCharacter(CharacterBase character, GameObject effect)
     {
         if (character.gameObject.layer != LayerMask.NameToLayer("Player"))
@@ -67,6 +93,11 @@ public abstract class SkillRunnerBase : ScriptableObject
         effect.SetActive(true);
     }
 
+    /// <summary>
+    /// array의 null체크, 길이 체크(0인지 아닌지), element의 null체크를 수행한다.
+    /// </summary>
+    /// <param name="array">대상 배열</param>
+    /// <returns>각종 체크를 수행하여 통과하면 true</returns>
     protected bool IsValidArray(GameObject[] array)
     {
         return array != null && array.Length > 0 && !Array.Exists(array, (value) => value == null);
@@ -80,7 +111,7 @@ public abstract class SkillRunnerBase : ScriptableObject
         foreach (ParticleSystem effect in managedEffects)
         {
             // 무한반복이 아니고 자식 중 하나라도 살아 있다면
-            if(!effect.main.loop && effect.IsAlive(true))
+            if (!effect.main.loop && effect.IsAlive(true))
             {
                 waitingEffects.Add(effect);
                 continue;
@@ -92,5 +123,86 @@ public abstract class SkillRunnerBase : ScriptableObject
 
         // 삭제되지 않은 이펙트 대기 후 삭제
         character.StartCoroutine(WaitForEffectEnded(waitingEffects));
+    }
+
+    /// <summary>
+    /// 충돌을 고려하여 Target Position(최종 위치)을 구한다.
+    /// 충돌은 Wall Layer만 고려한다.
+    /// </summary>
+    /// <param name="initialPos">초기 위치</param>
+    /// <param name="direction">캐릭터의 방향(좌우)</param>
+    /// <param name="targetPos">현재 계산된 목표 위치</param>
+    /// <param name="movingDistance">이동할 거리</param>
+    /// <returns>(targetPos - initialPos) 벡터가, Wall에 충돌한다면 Wall에서 일정 거리 앞, 아니라면 원래 위치를 반환</returns>
+    protected Vector3 GetTargetPosByCoillision(Vector3 initialPos, Vector3 direction, Vector3 targetPos, float movingDistance)
+    {
+        // 도착 지점 갱신 with Wall
+        if (Physics.Raycast(new Ray(initialPos, direction), out RaycastHit wallHit, (targetPos - initialPos).magnitude, 1 << 13)) // 13은 Wall
+        {
+            movingDistance = (wallHit.distance - 0.6f) * 0.99f;
+            return initialPos + direction * movingDistance;
+        }
+
+        return targetPos;
+    }
+
+    /// <summary>
+    ///  SkillTarget에 해당하는 타겟 중 씬 내에서 유효한 타겟을 전부 가져온다.
+    /// </summary>
+    /// <param name="caster">스킬 시전자</param>
+    /// <param name="target">스킬 대상</param>
+    /// <returns>씬 내의 유효 스킬 대상을 전부 반환</returns>
+    protected List<GameObject> GetTargets(CharacterBase caster, Define.SkillTarget target)
+    {
+        List<GameObject> targets = new List<GameObject>();
+        switch (target)
+        {
+            case Define.SkillTarget.NONE:
+                break;
+            case Define.SkillTarget.PC:
+                targets.Add(CharacterManager.Instance.GetCharacter(1 << 11)[0]);
+                break;
+            case Define.SkillTarget.SELF:
+                targets.Add(caster.gameObject);
+                break;
+            case Define.SkillTarget.MON:
+                targets = CharacterManager.Instance.GetCharacter(1 << 10);
+                break;
+            case Define.SkillTarget.GROUND: // 미구현
+                break;
+            case Define.SkillTarget.ALL:
+                targets = CharacterManager.Instance.GetCharacter(1 << 10 | 1 << 11);
+                break;
+            default:
+                Debug.LogError("Invalid Target");
+                break;
+        }
+
+        return targets;
+    }
+
+    /// <summary>
+    /// objs 내의 요소 중에, target에 해당하는 GameObject만을 반환
+    /// </summary>
+    /// <param name="caster">스킬 시전자</param>
+    /// <param name="target">스킬 대상</param>
+    /// <param name="objs">검사할 GameObjects</param>
+    /// <returns>target인 objs내의 요소 일체</returns>
+    protected List<GameObject> GetExistingTargets(CharacterBase caster, Define.SkillTarget target, GameObject[] objs)
+    {
+        var targets = GetTargets(caster, target);
+        var existingTargets = new List<GameObject>();
+
+        foreach (var obj in objs)
+        {
+            // CharacterManager에 등록되지 않은 obj는 스킵
+            // Ground의 경우도 받아온 Ground 리스트 안에 존재하지 않는 경우 판별할 예정
+            if (!targets.Contains(obj)) { continue; }
+
+            // 등록된 obj만 추가
+            existingTargets.Add(obj);
+        }
+
+        return existingTargets;
     }
 }
