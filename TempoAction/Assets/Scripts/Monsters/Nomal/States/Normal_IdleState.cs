b96cbabb9fd6c2be99c13d66780643f0a89f8c37
Budy123 <kth5948@naver.com>
2024-10-28 10:32:18 +0900

    Fix : 가끔 일반 공격을 하지 않고 멈춰버리는 현상

--- TempoAction/Assets/Scripts/Monsters/Nomal/States/Normal_IdleState.cs
+++ TempoAction/Assets/Scripts/Monsters/Nomal/States/Normal_IdleState.cs
@@ -19,17 +19,14 @@ public class Normal_IdleState : Normal_State
 
         float distance = Vector3.Distance(_monster.transform.position, _monster.Target.position);
 
-        if (_monster.TryAttack())
+        // 공격 시도(스킬 or 일반)
+        if (!_monster.TryAttack())
         {
-            if (_monster.Ani.GetBool("Attack"))
+            // 공격 범위 밖, 인지 범위 내(추격)
+            if (distance > _monster.MonsterSt.AttackRange && distance <= _monster.PerceptionDistance * SkillData.cm2m)
             {
-                _monster.Ani.SetBool("Attack", false);
+                _monster.CurrentPerceptionState = Define.PerceptionType.TRACE;
             }
-        }
-        // 공격 범위 밖, 인지 범위 내(추격)
-        else if (distance > _monster.MonsterSt.AttackRange && distance <= _monster.PerceptionDistance * SkillData.cm2m)
-        {
-            _monster.CurrentPerceptionState = Define.PerceptionType.TRACE;
         }
     }
 
